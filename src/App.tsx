348cd3255425abef3ddfc723c0b21aebcb645a06
rodlferreira <rodriggo.lferreira@icloud.com>
2021-05-21 14:36:10 -0300

    estrutura da aplicaçao - Ignite chapter II

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
-import styled from 'styled-components'
-
-const Title = styled.h1 `
-  font-size: 64px;
-  color: #8257e6;
-`
+import { GlobalStyles } from "./assets/styles/global";
 
 
 export function App() {
   return (
     <div className="App">
-      <Title>Hello world</Title>
+      <h1>Hello world</h1>
+      <GlobalStyles />
     </div>
   );
 }
